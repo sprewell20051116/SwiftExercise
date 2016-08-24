e009bb6198ebf9d0062ce18810f500e9716f9b99
GIGIGUN <sprewell20051116@gmail.com>
2016-08-24 22:30:15 +0800

    Add Project03

---
+++ Pro03/Project03/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project03
+//
+//  Created by GIGIGUN on 8/24/16.
+//  Copyright © 2016 GIGIGUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    var dataArray: [String] = ["We", "Heart", "Swift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell");
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator;
+        cell.textLabel?.text = dataArray[indexPath.row];
+        return cell;
+    }
+
+
+}
+
